2a61e8937b78b2ae8c0ae5982758bdc0c1b87feb
Gabe <gabessj4@gmail.com>
2024-12-07 21:08:29 -0400

    timer done i think

--- app/src/main/java/com/MOBI3002/in_progress/composables/Timer.kt
+++ app/src/main/java/com/MOBI3002/in_progress/composables/Timer.kt
@@ -1,8 +1,50 @@
 package com.MOBI3002.in_progress.composables
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.delay
 
 @Composable
 fun Timer(){
+    var timeLeft by remember { mutableStateOf(60) }
+    var isPaused by remember { mutableStateOf(false) }
 
+    LaunchedEffect (key1 = timeLeft) {
+        while (timeLeft > 0) {
+            delay(1000L)
+            timeLeft--
+        }
+    }
+
+
+
+    fun resetTimer() {
+        timeLeft = 60
+        isPaused = false
+    }
+
+    Column {
+        Text(text = "Time left: $timeLeft")
+        Button(onClick = { isPaused = !isPaused }) {
+            Text(text = if (isPaused) "Resume" else "Pause")
+        }
+        Button(onClick = { resetTimer() }) {
+            Text(text = "Reset")
+        }
+    }
+
+}
+
+@Composable
+@Preview
+fun checkTimer(){
+    Timer()
 }
